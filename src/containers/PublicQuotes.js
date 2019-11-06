419c6d4c9eaeb69211858e915eacd9fd56693d19
Marco Abilio <marc.e.mailing@gmail.com>
2019-11-06 23:41:25 +0000

    reversed to regular func component

--- src/containers/PublicQuotes.js
+++ src/containers/PublicQuotes.js
@@ -1,23 +1,20 @@
 /* eslint-disable react/jsx-filename-extension */
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Quote from './Quote';
 
+const PublicQuotesManager = (props) => {
+  const { quotes } = props;
+  const publicQuotes = quotes != null ? quotes.map(quote => (
+    <Quote
+      key={quote.id}
+      quote={quote}
+    />
+  )) : <p id="no-public-quotes">There are no public quotes</p>;
 
-class PublicQuotesManager extends PureComponent {
-  render() {
-    const { quotes } = this.props;
-    const publicQuotes = quotes != null ? quotes.map(quote => (
-      <Quote
-        key={quote.id}
-        quote={quote}
-      />
-    )) : <p id="no-public-quotes">There are no public quotes</p>;
-
-    return publicQuotes;
-  }
-}
+  return publicQuotes;
+};
 
 PublicQuotesManager.defaultProps = {
   quotes: null,
@@ -34,4 +31,5 @@ const mapStateToProps = state => ({
 export default connect(
   mapStateToProps,
 )(PublicQuotesManager);
+// testing purposes
 export { PublicQuotesManager };
